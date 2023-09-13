5d28d2cf6b1d7cf479dcf222c95687c903306b47
renan-andrade-senai <renan.andrade@edu.sc.senai.br>
2023-09-13 14:20:38 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ListaCliente from './components/ListaCliente.vue';
+
+export default defineComponent({
+  name: "App",
+  components: {
+    ListaCliente
+  }
+})
+</script>
+
+<template>
+  <ListaCliente/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ListaCliente.vue
@@ -0,0 +1,45 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Email</th>
+                <th scope="col">CPF</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>000.000.000-00</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>000.000.000-00</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                <td>000.000.000-00</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "ListaCliente"
+})
+</script>
+
+<style></style>
